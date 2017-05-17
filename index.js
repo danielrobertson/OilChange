d98b8b19f46cc6af0da913b7a760f13cac0ef341
danielrobertson <danielrobertson733@gmail.com>
2017-05-16 22:14:49 -0500

    remove api key

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var request = require('request'); 
 var app = express();
 
-app.get('/vehicle/info/vin/:vin', function (req, res) {
-	var edmundsUrl = 'https://api.edmunds.com/api/vehicle/v2/vins/' + req.params.vin + '?fmt=json&api_key=' + "tvdncyxym7hdqdqd284pvdye"; 
+app.get('/api/vehicle/info/vin/:vin', function (req, res) {
+	var edmundsUrl = 'https://api.edmunds.com/api/vehicle/v2/vins/' + req.params.vin + '?fmt=json&api_key=' + process.env.EDMUNDS_API_KEY; 
   	request(edmundsUrl, function(error, response, body){
   		if(!error && response.statusCode == 200) {
   			res.send(JSON.parse(body).make.name); 
