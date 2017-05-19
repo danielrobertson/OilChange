04070fb9691199b15c4543187f759f50785d1e4f
danielrobertson <danielrobertson733@gmail.com>
2017-05-19 16:22:24 -0500

    Vue front-end hello world

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var express = require('express');
 var request = require('request'); 
 var app = express();
 
+// get vehicle 
 app.get('/api/vehicle/info/vin/:vin', function (req, res) {
 	var edmundsUrl = 'https://api.edmunds.com/api/vehicle/v2/vins/' + req.params.vin + '?fmt=json&api_key=' + process.env.EDMUNDS_API_KEY; 
   	request(edmundsUrl, function(error, response, body){
   		if(!error && response.statusCode == 200) {
+        console.log(JSON.parse(body)); 
   			res.send(JSON.parse(body).make.name); 
   			
   		}
@@ -16,5 +18,5 @@ var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Listening at http://%s:%s", host, port)
 });
